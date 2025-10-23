ad33f3050357119ca36053d3fb7d84220f9cabf1
Pranjali-12 <kasbalepranjali@gmail.com>
2025-10-24 01:38:18 +0530

    working on UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_crud.DTOs;
+using dotnet_crud.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_crud.Controllers
 {
-    public class UserController
+    public class UserController(SignInManager<User> signInManager):BaseApiController
     {
-        
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterDto>> Register(RegisterDto registerDto)
+        {
+            return registerDto;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,64 @@
+// using dotnet_crud.Data;
+// using Microsoft.EntityFrameworkCore;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddControllers();
+// builder.Services.AddDbContext<StoreContext>(opt =>
+// {
+//     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+
+// // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+// app.UseHttpsRedirection();
+
+
+// app.Run();
 using dotnet_crud.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.Services.AddControllers();
+
+// Configure EF Core with SQL Server
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add OpenAPI (Swagger)
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    // In development, HTTPS redirection can be optional
+    // Uncomment if you have HTTPS configured in launchSettings.json
+    // app.UseHttpsRedirection();
+}
+else
+{
+    // In production, always redirect HTTP to HTTPS
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization(); // optional if you have auth
 
+app.MapControllers(); // map controller endpoints
 
 app.Run();
